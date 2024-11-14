fb9e21b64205eccf224b7c65cc191df64de50df4
Eymen <149965032+Eymen585@users.noreply.github.com>
2024-11-14 19:22:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,13 @@ gray_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
 
 _, thresh_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY_INV)
 
-# Görüntüyü ekrana gösterin
 cv2.imshow('Thresholded Image', thresh_image)
 
-# Tesseract ile metin tanıma
 extracted_text = pytesseract.image_to_string(thresh_image)
 
 print("Tanınan Metin: ")
 print(extracted_text)
 
-# Görüntü üzerinde tanınan metni ekleyin
 cv2.putText(image, extracted_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 cv2.imshow('El Yazisi Tanima', image)
